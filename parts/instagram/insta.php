b671e33af523f11583c8701ec3e46eb261cd004b
Martijn <m.reeuwijk@live.nl>
2016-01-11 21:16:07 +0100

    made the floats and the hover work

--- parts/instagram/insta.php
+++ parts/instagram/insta.php
@@ -1,20 +1,75 @@
 <section class="brt menuholder fs" id="door">
 
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
-  <article class="foodimage"></article>
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
+
+  <article class="foodimage">
+    <article class="tekst">
+      <p>Hi</p>
+    </article>
+  </article>
 
 </section>
